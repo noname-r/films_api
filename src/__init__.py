b1c1e476a525c294ab09ecaf24a8ec3a78585cae
noname-r <noname-r@ya.ru>
2021-04-22 16:50:48 +0300

    2

--- src/__init__.py
+++ src/__init__.py
@@ -1,11 +1,10 @@
+from flask import Flask, render_template, request
+from flask_migrate import Migrate
+from flask_restful import Api
+from flask_sqlalchemy import SQLAlchemy, get_debug_queries
 from flask_swagger_ui import get_swaggerui_blueprint
 
 import config
-from flask import Flask
-from flask_migrate import Migrate
-from flask_restful import Api
-from flask_sqlalchemy import SQLAlchemy
-
 
 app = Flask(__name__)
 app.config.from_object(config.Config)
@@ -30,11 +29,30 @@ def index():
     return render_template('index.html')
 
 @app.route('/greeting', methods=['POST'])
-def greeteng():
+def greeting():
     name=request.form.get('name')
     if not name:
         return 'Please, enter a value', 400
     return render_template('greeting.html', name=name)
 
-from . import routes, models
-#from src import routes
+
+#app.debug = True
+
+
+def sql_debug(response):
+    queries = list(get_debug_queries())
+    total_duration = 0.0
+    for q in queries:
+        total_duration += q.duration
+
+    print('=' * 80)
+    print(' SQL Queries - {0} Queries Executed in {1}ms'.format(len(queries), round(total_duration * 1000, 2)))
+    print('=' * 80)
+
+    return response
+
+
+app.after_request(sql_debug)
+
+from src import routes
+from src.database import models
---
+++ src/database/models.py
@@ -0,0 +1,50 @@
+import uuid
+
+from src import db
+
+movies_actors = db.Table(
+    'movies_actors',
+    db.Column('actor_id', db.Integer, db.ForeignKey('actors.id'), primary_key=True),
+    db.Column('film_id', db.Integer, db.ForeignKey('films.id'), primary_key=True)
+)
+
+class Film(db.Model):
+    __tablename__ = 'films'
+
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String, nullable=False)
+    release_date = db.Column(db.Date, index=True, nullable=False)
+    uuid = db.Column(db.String(36), unique=True)
+    description = db.Column(db.Text)
+    distributed_by = db.Column(db.String(128), nullable=False)
+    length = db.Column(db.Float)
+    rating = db.Column(db.Float)
+    actors = db.relationship('Actor', secondary=movies_actors, lazy='subquery', backref=db.backref('films', lazy=True))
+
+    def __init__(self, title, release_date, description, distributed_by, length, rating, actors=None):
+        self.title = title
+        self.release_date = release_date
+        self.uuid = str(uuid.uuid4())
+        self.description = description
+        self.distributed_by = distributed_by
+        self.length = length
+        self.rating = rating
+        if not actors:
+            self.actors = []
+        else:
+            self.actors = actors
+
+    def __repr__(self):
+        return f'Film({self.title}, {self.release_date}, {self.uuid}, {self.distributed_by}'
+
+
+class Actor(db.Model):
+    __tablename__ = 'actors'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), unique=True, nullable=False)
+    birthday = db.Column(db.Date)
+    is_active = db.Column(db.Boolean, default=False)
+
+    def __repr__(self):
+        return f'Actor({self.name}, {self.birthday})'
